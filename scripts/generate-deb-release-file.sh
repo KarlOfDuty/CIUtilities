3e48c7f9dd99645b274255a18f6cc3ac93feeda8
Karl Essinger <xkaess22@gmail.com>
2025-04-05 22:30:47 +0200

    Add script to generate deb release files

---
+++ scripts/generate-deb-release-file.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+set -e
+
+# Copied from https://earthly.dev/blog/creating-and-hosting-your-own-deb-packages-and-apt-repo/
+
+do_hash() {
+    HASH_NAME=$1
+    HASH_CMD=$2
+    echo "${HASH_NAME}:"
+    for f in $(find -type f); do
+        f=$(echo $f | cut -c3-) # remove ./ prefix
+        if [ "$f" = "Release" ]; then
+            continue
+        fi
+        echo " $(${HASH_CMD} ${f}  | cut -d" " -f1) $(wc -c $f)"
+    done
+}
+
+cat << EOF
+Origin: karlofduty.com
+Label: KarlofDuty
+Suite: debian
+Codename: debian
+Version: 1.0
+Architectures: amd64
+Components: dev
+Description: Repository for KarlofDuty made packages
+Date: $(date -Ru)
+EOF
+do_hash "MD5Sum" "md5sum"
+do_hash "SHA1" "sha1sum"
+do_hash "SHA256" "sha256sum"
